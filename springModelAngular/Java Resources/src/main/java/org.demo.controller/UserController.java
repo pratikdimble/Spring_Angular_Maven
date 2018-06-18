eada1f9cde2aebae4ae9a2c13b8c13a84367bde4
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-06-18 12:10:52 +0530

    Create UserController.java

---
+++ springModelAngular/Java Resources/src/main/java/org.demo.controller/UserController.java
@@ -0,0 +1,32 @@
+package org.demo.controller;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	@Autowired
+	EmployeeService empService;
+
+	@RequestMapping(value="/")
+	public String landing(HttpServletRequest req,HttpServletResponse res){
+		return "/tableSort";
+	}//landing() close
+	
+
+
+}
