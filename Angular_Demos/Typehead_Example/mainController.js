ae7810c854c3525d14d35e1e2da7e99722bdbf48
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-07-27 11:50:26 +0530

    Update mainController.js

--- Angular_Demos/Typehead_Example/mainController.js
+++ Angular_Demos/Typehead_Example/mainController.js
@@ -2,12 +2,13 @@ angular.module('myApp', ['ui.bootstrap'])
     .controller("mainCtrl", function ($scope) {
     $scope.selected = '';
     $scope.states = [{id:1,postcode:'411046',address:'Katraj'},
-    									{id:2,postcode:'412205',address:'Bhor'},
-    									{id:3,postcode:'412206',address:'Saswad'},
-    									{id:4,postcode:'412207',address:'Shirwal'},
-    									{id:5,postcode:'412208',address:'Mulashi'}];
+    				{id:2,postcode:'412205',address:'Bhor'},
+    				{id:3,postcode:'412206',address:'Saswad'},
+    				{id:4,postcode:'412207',address:'Shirwal'},
+    				{id:5,postcode:'412208',address:'Mulashi'}];
     
      $scope.setPcode = function(site) {
+      $scope.setPincode = site.postcode;
   		$scope.setId = site.id;
   	};
 });
