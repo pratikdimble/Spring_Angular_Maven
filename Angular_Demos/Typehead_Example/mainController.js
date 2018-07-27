7abbe8f228b65b70739e6ee496bee8ca52cb8437
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-07-27 11:33:12 +0530

    Create mainController.js

---
+++ Angular_Demos/Typehead_Example/mainController.js
@@ -0,0 +1,13 @@
+angular.module('myApp', ['ui.bootstrap'])
+    .controller("mainCtrl", function ($scope) {
+    $scope.selected = '';
+    $scope.states = [{id:1,postcode:'411046',address:'Katraj'},
+    									{id:2,postcode:'412205',address:'Bhor'},
+    									{id:3,postcode:'412206',address:'Saswad'},
+    									{id:4,postcode:'412207',address:'Shirwal'},
+    									{id:5,postcode:'412208',address:'Mulashi'}];
+    
+     $scope.setPcode = function(site) {
+  		$scope.setId = site.id;
+  	};
+});
